17f0f54f62639f904709f85b4744bf5e0f02d662
jgaffuri <jgaf@proton.me>
2022-12-21 14:44:34 +0100

    a

---
+++ webpack_parquet_ddb/src/index.js
@@ -0,0 +1,23 @@
+import * as ddb from "duckdb"
+
+const url = "https://raw.githubusercontent.com/eurostat/gridviz/master/assets/parquet/building_area.parquet"
+console.log(url)
+console.log("aaa")
+
+
+//https://www.npmjs.com/package/duckdb
+//https://duckdb.org/docs/api/nodejs/overview
+//https://duckdb.org/docs/data/parquet
+//https://duckdb.org/docs/guides/import/http_import
+
+
+//var db = new duckdb.Database(':memory:');
+
+/*
+db.all('SELECT 42 AS fortytwo', function(err, res) {
+    if (err) {
+      throw err;
+    }
+    console.log(res[0].fortytwo)
+  });
+*/
